7a8824790c4fbce1e1342a370693bf6d311cfea8
Kamei Shogo <km45@users.noreply.github.com>
2020-01-02 23:53:51 +0900

    Add test for task version in role python

--- src/playbooks/roles/python/molecule/default/tests/test_default.py
+++ src/playbooks/roles/python/molecule/default/tests/test_default.py
@@ -1,5 +1,7 @@
 import os
 
+import parse
+
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -13,3 +15,14 @@ def test_pyenv_command_existence(host):
 
         assert host.check_output(
             ". /home/vagrant/.bashrc.d/pyenv.bashrc && which pyenv")
+
+
+def test_python_version(host):
+    with host.sudo("vagrant"):
+        assert host.check_output("whoami") == "vagrant"
+
+        result = parse.parse(
+            "Python {version}", host.check_output("python --version 2>&1"))
+        assert result is not None
+
+        assert result['version'] == "3.7.3"
