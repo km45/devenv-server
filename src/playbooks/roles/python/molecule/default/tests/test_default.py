ad69336d8d51b8ac936adb7f287f9eb729b27b47
Kamei Shogo <km45@users.noreply.github.com>
2020-01-02 22:18:30 +0900

    Migrate pyenv installation to new role python

---
+++ src/playbooks/roles/python/molecule/default/tests/test_default.py
@@ -0,0 +1,14 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_pyenv_command_existence(host):
+    with host.sudo("vagrant"):
+        assert host.check_output("whoami") == "vagrant"
+
+        assert host.check_output(". /home/vagrant/.bashrc.d/pyenv.bashrc && which pyenv")
