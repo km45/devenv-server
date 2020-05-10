66b26263a6ad51587ffaf18f064eb712bba9a741
Kamei Shogo <km45@users.noreply.github.com>
2020-05-11 04:28:07 +0900

    Execute test parallelly on github actions

---
+++ test-all.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -eu
+
+readonly PARALLEL_SUMMARY_FILE="test-all-summary.log"
+
+function main() {
+    rm -rf test-all.log test-all-results
+
+    set +e
+    parallel \
+        --eta \
+        --joblog ${PARALLEL_SUMMARY_FILE} \
+        make test ROLE={} ::: \
+            cplusplus \
+            git \
+            gui-applications \
+            nkf \
+            python \
+            shellcheck \
+            vscode \
+            xmllint \
+            zip
+    local parallel_retval=$?
+    set -e
+
+
+    # https://stackoverflow.com/questions/5917413/concatenate-multiple-files-but-include-filename-as-section-headers
+    cat ${PARALLEL_SUMMARY_FILE}
+    # tail -n +1 ${PARALLEL_RESULT_DIRECTORY}/1/*/*
+
+    return ${parallel_retval}
+}
+
+main "$@"
