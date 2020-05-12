55473353dbc281e33aa2e66dffa0d3dd17ab4008
Kamei Shogo <km45@users.noreply.github.com>
2020-05-12 19:21:45 +0900

    Print all output for test

--- test-all.sh
+++ test-all.sh
@@ -2,6 +2,7 @@
 set -eu
 
 readonly PARALLEL_SUMMARY_FILE="test-all-summary.log"
+readonly PARALLEL_RESULT_DIRECTORY="test-all-results"
 
 function main() {
     rm -f ${PARALLEL_SUMMARY_FILE}
@@ -10,6 +11,7 @@ function main() {
     parallel \
         --eta \
         --joblog ${PARALLEL_SUMMARY_FILE} \
+        --results ${PARALLEL_RESULT_DIRECTORY} \
         make test ROLE={} ::: \
             cplusplus \
             git \
@@ -23,10 +25,8 @@ function main() {
     local parallel_retval=$?
     set -e
 
-
     # https://stackoverflow.com/questions/5917413/concatenate-multiple-files-but-include-filename-as-section-headers
-    cat ${PARALLEL_SUMMARY_FILE}
-    # tail -n +1 ${PARALLEL_RESULT_DIRECTORY}/1/*/*
+    tail -n +1 cat ${PARALLEL_SUMMARY_FILE} ${PARALLEL_RESULT_DIRECTORY}/1/*/*
 
     return ${parallel_retval}
 }
